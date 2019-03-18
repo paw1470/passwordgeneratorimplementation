c06fb79eabba6d3e46f2a4d8cff68e72152a71ec
dominisz <dominisz@gmail.com>
2019-03-18 20:04:49 +0100

    Initial commit

---
+++ src/main/java/pl/dominisz/passwordgeneratorimplementation/PasswordGeneratorImplementation.java
@@ -0,0 +1,16 @@
+package pl.dominisz.passwordgeneratorimplementation;
+
+import pl.dominisz.passwordgeneratorapi.PasswordGeneratorParameters;
+import pl.dominisz.passwordgeneratorapi.PasswordGeneratorService;
+
+import java.util.List;
+
+public class PasswordGeneratorImplementation implements PasswordGeneratorService {
+    public String generate(PasswordGeneratorParameters passwordGeneratorParameters) {
+        return null;
+    }
+
+    public List<String> generate(PasswordGeneratorParameters passwordGeneratorParameters, int i) {
+        return null;
+    }
+}
