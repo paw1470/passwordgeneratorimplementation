0885651aaa521547776e0b2a1c7add8eb91748bf
Piotr Zdanowski <piotr.zdanowski@outlook.com>
2019-04-08 18:53:01 +0200

    Implemented generation in uppercase, symbols and numbers

--- src/main/java/pl/dominisz/passwordgeneratorimplementation/PasswordGeneratorImplementation.java
+++ src/main/java/pl/dominisz/passwordgeneratorimplementation/PasswordGeneratorImplementation.java
@@ -10,14 +10,47 @@ public class PasswordGeneratorImplementation implements PasswordGeneratorService
 
     private static final Random RANDOM = new Random();
     private static final String LOWERCASE_CHARACTERS = "qwertyuiopasdfghjklzxcvbnm";
+    private static final String UPPERCASE_CHARACTERS = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    private static final String SYMBOLS = "!@#$%^&*()-=+";
+    private static final String NUMBERS = "0123456789";
 
     public String generate(PasswordGeneratorParameters passwordGeneratorParameters) {
+        if(isInvalid(passwordGeneratorParameters)){
+            throw new IllegalArgumentException();
+        }
+        String alphabet = generateAlphabet(passwordGeneratorParameters);
         String password = "";
         for (int i = 0; i < passwordGeneratorParameters.getPasswordLength(); i++) {
-            password += LOWERCASE_CHARACTERS.charAt(RANDOM.nextInt(LOWERCASE_CHARACTERS.length()));
+            password += alphabet.charAt(RANDOM.nextInt(alphabet.length()));
         }
         return password;
     }
+
+    private String generateAlphabet(PasswordGeneratorParameters passwordGeneratorParameters) {
+        String alphabet = "";
+        if(passwordGeneratorParameters.isIncludeLowercaseCharacters()){
+            alphabet += LOWERCASE_CHARACTERS;
+        }
+        if(passwordGeneratorParameters.isIncludeUppercaseCharacters()){
+            alphabet += UPPERCASE_CHARACTERS;
+        }
+        if(passwordGeneratorParameters.isIncludeSymbols()){
+            alphabet += SYMBOLS;
+        }
+        if(passwordGeneratorParameters.isIncludeNumbers()){
+            alphabet += NUMBERS;
+        }
+        return alphabet;
+    }
+
+    private boolean isInvalid(PasswordGeneratorParameters passwordGeneratorParameters) {
+        return passwordGeneratorParameters.getPasswordLength() <= 0
+                || !(passwordGeneratorParameters.isIncludeLowercaseCharacters()
+                || passwordGeneratorParameters.isIncludeNumbers()
+                || passwordGeneratorParameters.isIncludeSymbols()
+                || passwordGeneratorParameters.isIncludeUppercaseCharacters());
+    }
+
 
     public List<String> generate(PasswordGeneratorParameters passwordGeneratorParameters, int count) {
         return null;
--- src/test/java/pl/dominisz/passwordgeneratorimplementation/PasswordGeneratorImplementationTest.java
+++ src/test/java/pl/dominisz/passwordgeneratorimplementation/PasswordGeneratorImplementationTest.java
@@ -2,6 +2,7 @@ package pl.dominisz.passwordgeneratorimplementation;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import pl.dominisz.passwordgeneratorapi.PasswordGeneratorParameters;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,8 +14,76 @@ class PasswordGeneratorImplementationTest {
     @Test
     void shouldGeneratePasswordOfProperLength() {
         PasswordGeneratorParameters passwordGeneratorParameters = new PasswordGeneratorParameters();
+        passwordGeneratorParameters.setIncludeUppercaseCharacters(true);
+        passwordGeneratorParameters.setIncludeLowercaseCharacters(true);
+        passwordGeneratorParameters.setIncludeNumbers(true);
+        passwordGeneratorParameters.setIncludeSymbols(true);
+
         passwordGeneratorParameters.setPasswordLength(10);
+
         String password = passwordGeneratorImplementation.generate(passwordGeneratorParameters);
+
         Assertions.assertEquals(10, password.length());
+    }
+
+    @Test
+    void shouldGenerateLowercasePassword(){
+        PasswordGeneratorParameters passwordGeneratorParameters = new PasswordGeneratorParameters();
+        passwordGeneratorParameters.setIncludeLowercaseCharacters(true);
+        passwordGeneratorParameters.setPasswordLength(10);
+
+        String password = passwordGeneratorImplementation.generate(passwordGeneratorParameters);
+
+        Assertions.assertEquals(10, password.length());
+        Assertions.assertEquals(password, password.toLowerCase());
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentException(){
+        PasswordGeneratorParameters passwordGeneratorParameters = new PasswordGeneratorParameters();
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {passwordGeneratorImplementation.generate(passwordGeneratorParameters);});
+    }
+
+    @Test
+    void shouldGenerateUppercasePassword(){
+        PasswordGeneratorParameters passwordGeneratorParameters = new PasswordGeneratorParameters();
+        passwordGeneratorParameters.setIncludeUppercaseCharacters(true);
+        passwordGeneratorParameters.setPasswordLength(10);
+
+        String password = passwordGeneratorImplementation.generate(passwordGeneratorParameters);
+
+        Assertions.assertEquals(10, password.length());
+        Assertions.assertEquals(password, password.toUpperCase());
+    }
+
+    @Test
+    void shouldGenerateSymbolsPassword(){
+        PasswordGeneratorParameters passwordGeneratorParameters = new PasswordGeneratorParameters();
+        passwordGeneratorParameters.setIncludeSymbols(true);
+        passwordGeneratorParameters.setPasswordLength(10);
+
+        String password = passwordGeneratorImplementation.generate(passwordGeneratorParameters);
+
+        Assertions.assertTrue(containsOnlySymbols(password));
+    }
+
+    private boolean containsOnlySymbols(String string){
+        return string.matches("[!@#$%^&*()\\-=+]+");
+    }
+
+    @Test
+    void shouldGenerateNumericPassword(){
+        PasswordGeneratorParameters passwordGeneratorParameters = new PasswordGeneratorParameters();
+        passwordGeneratorParameters.setIncludeNumbers(true);
+        passwordGeneratorParameters.setPasswordLength(10);
+
+        String password = passwordGeneratorImplementation.generate(passwordGeneratorParameters);
+
+        Assertions.assertTrue(containsOnlyNumbers(password));
+    }
+
+    private boolean containsOnlyNumbers(String password) {
+        return password.matches("[0-9]+");
     }
 }
